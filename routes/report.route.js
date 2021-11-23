9c569e48659646ef73bf3b40e009024a95df8d62
IlyaKamovich <neovlad96@gmail.com>
2021-11-23 16:37:03 +0300

    Revert "Merge commits:"
    This reverts commit fa50d556b3a09499a352345aaed9567eba640841.

--- routes/report.route.js
+++ routes/report.route.js
@@ -1,18 +1,9 @@
 import express from "express";
-<<<<<<< HEAD
 import { addNewReport, getReports } from "../controllers/report.controller.js";
 
 const router = express.Router();
 
 router.post("/report", addNewReport);
 router.get("/reports/sourse", getReports);
-=======
-import { createReport, getReports } from "../controllers/report.controller.js";
-
-const router = express.Router();
-
-router.post("/createReport", createReport);
-router.get("/getReports", getReports);
->>>>>>> 1d586dc (Added routes folder, fixed filenames and controllers)
 
 export default router;
--- routes/targetolog.route.js
+++ routes/targetolog.route.js
@@ -1,16 +1,8 @@
 import express from "express";
-<<<<<<< HEAD
 import { addNewTargetolog } from "../controllers/targetolog.controller.js";
 
 const router = express.Router();
 
 router.post("/targetolog", addNewTargetolog);
-=======
-import { createTargetolog } from "../controllers/targetolog.controller.js";
-
-const router = express.Router();
-
-router.post("/createTargetolog", createTargetolog);
->>>>>>> 1d586dc (Added routes folder, fixed filenames and controllers)
 
 export default router;
