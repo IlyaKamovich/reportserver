fd66ad036e81b2a3ea7e20674fbed74ff81ee883
IlyaKamovich <neovlad96@gmail.com>
2021-11-24 18:01:39 +0300

    Revert "Added typescript"

--- routes/report.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addNewReport, getReports } from "../controllers/report.controller";
-
-const router = express.Router();
-
-router.post("/report", addNewReport);
-router.get("/reports/sourse", getReports);
-
-export default router;
