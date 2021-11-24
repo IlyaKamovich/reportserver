207c33287e30e60c453dd03ceb7bde4fe2341b37
IlyaKamovich <kamovichilya@gmail.com>
2021-11-24 18:02:09 +0300

    Revert "Added typescript" (#6)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import mongoose from "mongoose";
-import reportRoutes from "./routes/report.route";
-import targetologRoutes from "./routes/targetolog.route";
+import reportRoutes from "./routes/report.route.js";
+import targetologRoutes from "./routes/targetolog.route.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -18,9 +18,12 @@ app.use(targetologRoutes);
 
 const start = async () => {
   try {
-    await mongoose.connect("mongodb+srv://ilya:eazykatka@cluster0.u1x5r.mongodb.net/myFirstDatabase");
+    await mongoose.connect("mongodb+srv://ilya:eazykatka@cluster0.u1x5r.mongodb.net/myFirstDatabase", {
+      useNewUrlParser: true,
+    });
+
     app.listen(port, () => console.log(`Server running on port ${port}`));
-  } catch (e: any) {
+  } catch (e) {
     console.log(e.message);
   }
 };
--- models/tagretolog.model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { ITargetolog } from "../interfaces/targetolog.interface";
-
-const TargetologSchema = new mongoose.Schema<ITargetolog>({
-  name: { type: String, required: true },
-  source: { type: String, required: true },
-});
-
-export const Targetolog = mongoose.model<ITargetolog>("targetolog", TargetologSchema);
