1c8125d984b9842d089076dfd330ad357e5afba2
IlyaKamovich <neovlad96@gmail.com>
2021-11-26 14:47:56 +0300

    Remove duplicated files

--- routes/targetolog.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addNewTargetolog } from "../controllers/targetolog.controller.js";
-
-const router = express.Router();
-
-router.post("/targetolog", addNewTargetolog);
-
-export default router;
